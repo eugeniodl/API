ce8dec10ec5e728a81f2ec0bb79de2b10ab73757
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2025-05-27 11:26:04 -0600

    UsersController

---
+++ EjemploAPI01/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using EjemploAPI01.Data;
+using EjemploAPI01.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EjemploAPI01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly SampleDbContext _context;
+
+        public UsersController(SampleDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.UserId }, user);
+        }
+    }
+}
