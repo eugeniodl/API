5732af820ea382da6e95425853caa760731984e4
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2025-06-03 10:26:43 -0600

    Inyección de dependencias

---
+++ School_API/Repositories/IRepositories/IStudentRepository.cs
@@ -0,0 +1,9 @@
+﻿using SharedModels;
+
+namespace School_API.Repositories.IRepositories
+{
+    public interface IStudentRepository : IRepository<Student>
+    {
+        Task<Student> UpdateAsync(Student student);
+    }
+}
